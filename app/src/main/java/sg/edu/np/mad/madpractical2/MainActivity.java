9c283dbf6ea63666f4e4ea248bf3a02c184877e6
jining <s10223499@connect.np.edu.sg>
2023-05-08 15:49:54 +0800

    Updated

--- app/src/main/java/sg/edu/np/mad/madpractical2/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical2/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,10 +24,44 @@ public class MainActivity extends AppCompatActivity {
         myUser.userFollowed = false;
         myUser.isUserFollowed();
 
+        TextView tv = findViewById(R.id.textView2);
+        tv.setText("Hello World!");
+
         TextView tv1 = findViewById(R.id.textView);
         tv1.setText("Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua");
 
+        ToggleButton togBut1 = findViewById(R.id.toggleButton);
+        togBut1.setText("Follow");
 
+        if(myUser.userFollowed){
+            togBut1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    togBut1.getTextOff();
+                    Log.v(TITLE, "Toggle Button: Follow clicked!");
+                }
+            });
+        }
+        else{
+            togBut1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    togBut1.getTextOn();
+                    Log.v(TITLE, "Toggle Button: Follow not clicked!");
+                }
+            });
+        }
+
+        ToggleButton togBut2 = findViewById(R.id.toggleButton3);
+        togBut2.setText("Message");
+
+        togBut2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                togBut2.getTextOn();
+                Log.v(TITLE, "Toggle Button: Message clicked!");
+            }
+        });
     }
 
     @Override
